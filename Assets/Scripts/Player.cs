2ee6e9e6c72d522923cd2361eb4f3fc214b870f1
roor48 <kmgoio@naver.com>
2024-01-04 21:07:40 +0900

    asdf

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace A
+{
+    public class Player : MonoBehaviour
+    {
+        CharacterController characterController;
+        public float speed = 2f;
+        Animator animator;
+        bool isWalk = false;
+
+        private void Start()
+        {
+            characterController = this.GetComponent<CharacterController>();
+            animator = this.GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            Walk();
+            Attack();
+        }
+
+        readonly int isWalkHash = Animator.StringToHash("isWalk");
+        readonly int isAttackHash = Animator.StringToHash("isAttack");
+        private void Walk()
+        {
+            isWalk = false;
+
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            if (horizontal != 0 || vertical != 0)
+            {
+                characterController.Move(speed * Time.deltaTime * (this.transform.right * horizontal + this.transform.forward * vertical).normalized);
+                isWalk = true;
+            }
+
+            animator.SetBool(isWalkHash, isWalk);
+        }
+        private void Attack()
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                animator.SetTrigger(isAttackHash);
+            }
+        }
+    }
+}
